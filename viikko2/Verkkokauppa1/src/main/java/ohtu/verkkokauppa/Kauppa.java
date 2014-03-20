5fa2ea1742658a27d375b8dc654c3898238ce53e
Eveliina <pevetiainen.ep@hotmail.com>
2014-03-20 17:16:15 +0200

    Testi annotaatiobranch

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,5 +1,9 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kauppa {
 
     private VarastoRajap varasto;
@@ -7,7 +11,8 @@ public class Kauppa {
     private Ostoskori ostoskori;
     private ViitegeneraattoriRajap viitegeneraattori;
     private String kaupanTili;
-
+    
+    @Autowired
     public Kauppa(PankkiRajap pankki, VarastoRajap varasto, 
             ViitegeneraattoriRajap viitegeneraattori) {
         this.varasto = varasto;
