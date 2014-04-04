db680405a74a6a82eb33c19c4bcd386805ac840a
Eveliina <pevetiainen.ep@hotmail.com>
2014-04-04 19:34:50 +0300

    VKO4 T8910

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -6,26 +6,108 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
+
     public static void main(String[] args) {
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:8090");
-        System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click(); 
-        
+        System.out.println(driver.getPageSource());
+//        WebElement element = driver.findElement(By.linkText("login"));       
+//        element.click(); 
+//        
+//        System.out.println("==");
+//        
+//        System.out.println( driver.getPageSource() );
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//        
+//        System.out.println("==");
+//        System.out.println( driver.getPageSource() );
+
+        //Epaonnistunut kirjautuminen - oikea tunnus vaara salasana
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        System.out.println("==");
+//
+//        System.out.println(driver.getPageSource());
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkepp");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//
+//        System.out.println("==");
+//        System.out.println(driver.getPageSource());
+                //Epaonnistunut kirjautuminen - ei tunnusta
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        System.out.println("==");
+//
+//        System.out.println(driver.getPageSource());
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("tetta");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkepp");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//
+//        System.out.println("==");
+//        System.out.println(driver.getPageSource());
+                //Uuden kayttajatunnuksen luominen
+//        WebElement element = driver.findElement(By.linkText("register new user"));
+//        element.click();
+//
+//        System.out.println("==");
+//
+//        System.out.println(driver.getPageSource());
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekkate");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("12345678a");
+//        element = driver.findElement(By.name("passwordConfirmation"));
+//        element.sendKeys("12345678a");
+//        element = driver.findElement(By.name("add"));
+//        element.submit();
+//
+//        System.out.println("==");
+//        System.out.println(driver.getPageSource());
+        //Uuden kayttajatunnuksen luominen
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
         System.out.println("==");
-        
-        System.out.println( driver.getPageSource() );
+
+        System.out.println(driver.getPageSource());
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("pekkate");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("add"));
         element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        WebElement element2 = driver.findElement(By.linkText("continue to application mainpage"));
+        element2.click();
         
         System.out.println("==");
-        System.out.println( driver.getPageSource() );
+        System.out.println(driver.getPageSource());
+//
+        WebElement element3 = driver.findElement(By.linkText("logout"));
+        element3.click();
         
+         System.out.println("==");
+        System.out.println(driver.getPageSource());
+
     }
 }
---
+++ viikko4/LoginWeb2/src/test/easyb/new_user_creation_Story.groovy
@@ -0,0 +1,212 @@
+
+import ohtu.*
+import ohtu.authentication.*
+import org.openqa.selenium.*
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+
+description """A new user account can be created 
+              if a proper unused username 
+              and a proper password are given"""
+
+scenario "creation succesfull with correct username and password", {
+    given 'command new user is selected', {
+       
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+    }
+ 
+    when 'a valid username and password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaa");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+    }
+
+    then 'new user is registered to system', {
+        driver.getPageSource().contains("Welcome to Ohtu App!").shouldBe true
+    }
+}
+
+scenario "can login with succesfully generated account", {
+    given 'command new user is selected', {
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+       
+    }
+ 
+    when 'a valid username and password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaab");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        WebElement element2 = driver.findElement(By.linkText("continue to application mainpage"));
+        element2.click();
+        
+        WebElement element3 = driver.findElement(By.linkText("logout"));
+        element3.click();
+        
+        element4 = driver.findElement(By.linkText("login"));       
+        element4.click();     
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaab");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("12345678a");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+
+    then  'new credentials allow logging in to system', {
+        driver.getPageSource().contains("Welcome to Ohtu Application!").shouldBe true
+    }
+}
+
+scenario "creation fails with correct username and too short password", {
+    given 'command new user is selected',{
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+      
+    }
+    when 'a valid username and too short password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaabc");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("123");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("123");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+    }
+    then 'new user is not be registered to system', {
+        driver.getPageSource().contains("length greater or equal to 8").shouldBe true
+    }
+}
+
+scenario "creation fails with correct username and pasword consisting of letters", {
+    given 'command new user is selected',{
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+      
+    }
+    when 'a valid username and password consisting of letters are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("asdfgh");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("abcdefgh");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("abcdefgh");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+    }
+    then 'new user is not be registered to system', {
+        driver.getPageSource().contains("must contain one character that is not a letter").shouldBe true
+    }
+}
+
+
+scenario "creation fails with too short username and valid pasword", {
+    given 'command new user is selected',{
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+      
+    }
+    when 'a too sort username and valid password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("asd");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+    }
+    then 'new user is not be registered to system', {
+        driver.getPageSource().contains("length 5-10").shouldBe true
+    }
+}
+
+scenario "creation fails with already taken username and valid pasword", {
+    given 'command new user is selected', {
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+      
+    }
+    when 'a already taken username and valid password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaab");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("add"));
+        element.submit();  
+    }
+    then 'new user is not be registered to system', {
+        driver.getPageSource().contains("username or password invalid").shouldBe true
+    }
+}
+
+scenario "can not login with account that is not succesfully created", {
+    given 'command new user is selected', {
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8090");
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+     
+    }
+    when 'a invalid username/password are entered', {
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaab");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        WebElement element2 = driver.findElement(By.linkText("back to home"));
+        element2.click();
+        
+        element4 = driver.findElement(By.linkText("login"));       
+        element4.click();     
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaab");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("abcdefgh1");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+    then  'new credentials do not allow logging in to system', {
+        driver.getPageSource().contains("wrong username or password").shouldBe true
+    }
+}
+
+
+
