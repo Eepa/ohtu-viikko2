44e67279850b715780877e2e953843c9c094d9d0
Eveliina <pevetiainen.ep@hotmail.com>
2014-04-09 13:31:15 +0300

    VKO5 T3

--- viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,10 +1,6 @@
 package ohtu;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//    }
-//}
+// selitystä http://stackoverflow.com/questions/18093928/what-does-could-not-find-or-load-main-class-mean
+
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,22 +12,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String studentNr = "014152724";
-        if ( args.length>0) {
+        if (args.length > 0) {
             studentNr = args[0]; // Saa komentoriviltä oppilasnumeron ensimmäisenä argumenttina
         }
 
-        String url = "http://ohtustats.herokuapp.com/students/"+studentNr+"/submissions";
+        String url = "http://ohtustats.herokuapp.com/students/" + studentNr + "/submissions";
 
         HttpClient client = new HttpClient();
         GetMethod method = new GetMethod(url);
         client.executeMethod(method);
 
-        InputStream stream =  method.getResponseBodyAsStream();
+        InputStream stream = method.getResponseBodyAsStream();
 
         String bodyText = IOUtils.toString(stream);
 
         System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
+        System.out.println(bodyText);
 
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
@@ -40,15 +36,15 @@ public class Main {
         for (Submission submission : subs) {
             System.out.println(submission);
         }
-        
+
         int tehtaviaYhteensa = 0;
         int tuntejaYhteensa = 0;
-        
-        for(Submission submission : subs){
+
+        for (Submission submission : subs) {
             tehtaviaYhteensa += submission.getTehtaviaYhteensa();
             tuntejaYhteensa += submission.getHours();
         }
-        
+
         System.out.print("Yhteensä: " + tehtaviaYhteensa + " tehtävää ja " + tuntejaYhteensa + " tuntia");
 
     }
