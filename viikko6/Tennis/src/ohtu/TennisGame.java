1774d289f63ef1047df274a2b7ca68abc93fb1a5
Eveliina <pevetiainen.ep@hotmail.com>
2014-04-16 11:03:37 +0300

    VKO6 T4

--- viikko6/Tennis/src/ohtu/TennisGame.java
+++ viikko6/Tennis/src/ohtu/TennisGame.java
@@ -23,91 +23,86 @@ public class TennisGame {
 
     }
 
-    public String returnEvenScore() {
-        String score = "";
+    public String returnEvenScore(int score) {
 
-        switch (player1Score) {
+        switch (score) {
             case 0:
-                score = "Love-All";
-                break;
+                return "Love-All";
+
             case 1:
-                score = "Fifteen-All";
-                break;
+                return "Fifteen-All";
+
             case 2:
-                score = "Thirty-All";
-                break;
+                return "Thirty-All";
+
             case 3:
-                score = "Forty-All";
-                break;
-            default:
-                score = "Deuce";
+                return "Forty-All";
 
         }
-        return score;
+        return "Deuce";
+    }
+
+    public String returnAdvantageOrWinningScore(int playerScoreDistance) {
+
+        if (playerScoreDistance == 1 || playerScoreDistance == -1) {
+            return this.returnAdvantageScore(playerScoreDistance);
+        }
+
+        return this.returnWinningScore(playerScoreDistance);
 
     }
 
-    public String returnAdvantageScore() {
-        String score = "";
-
-        int minusResult = player1Score - player2Score;
-        if (minusResult == 1) {
-            score = "Advantage player1";
-        } else if (minusResult == -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
+    public String returnAdvantageScore(int playerScoreDistance) {
+        if (playerScoreDistance == 1) {
+            return "Advantage player1";
         }
-        return score;
+        return "Advantage player2";
+
+    }
+
+    public String returnWinningScore(int playerScoreDistance) {
+        if (playerScoreDistance >= 2) {
+            return "Win for player1";
+        }
+        return "Win for player2";
+
     }
 
     public String returnUnevenScore() {
-        String score = "";
-        int tempScore = 0;
 
-        for (int i = 1; i < 3; i++) {
+        return this.getPlayerScore(player1Score) + "-" + this.getPlayerScore(player2Score);
 
-            if (i == 1) {
-                tempScore = player1Score;
-            } else {
-                score += "-";
-                tempScore = player2Score;
-            }
+    }
 
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+    public String getPlayerScore(int playerScore) {
+        switch (playerScore) {
+            case 0:
+                return "Love";
+
+            case 1:
+                return "Fifteen";
+
+            case 2:
+                return "Thirty";
+
+            case 3:
+                return "Forty";
+
         }
-
-        return score;
-
+        return "";
     }
 
     public String getScore() {
 
         if (player1Score == player2Score) {
-            return this.returnEvenScore();
+            return this.returnEvenScore(player1Score);
 
         } else if (player1Score >= 4 || player2Score >= 4) {
-            return this.returnAdvantageScore();
+            return this.returnAdvantageOrWinningScore(player1Score - player2Score);
 
-        } else {
-
-            return this.returnUnevenScore();
         }
+
+        return this.returnUnevenScore();
 
     }
 }
